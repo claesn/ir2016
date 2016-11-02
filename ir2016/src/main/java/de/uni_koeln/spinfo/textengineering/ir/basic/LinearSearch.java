214fdba155ec8abaaf86fc6645d4b1c07357330c
claesn <c.neuefeind@uni-koeln.de>
2016-11-02 16:01:52 +0100

    Fix linear search.

--- ir2016/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/LinearSearch.java
+++ ir2016/src/main/java/de/uni_koeln/spinfo/textengineering/ir/basic/LinearSearch.java
@@ -1,8 +1,9 @@
 package de.uni_koeln.spinfo.textengineering.ir.basic;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LinearSearch {
 
@@ -12,22 +13,26 @@ public class LinearSearch {
 		works = corpus.getWorks();
 	}
 
-	public List<Integer> search(String query) {
-		
-		List<Integer> result = new ArrayList<>(); 
-		
-		for (Work work : works) {
-			String text = work.getText();
-			List<String> tokens = Arrays.asList(text.split(" "));
-			for (String t : tokens) {
-				if(t.compareTo(query) == 0){
-					result.add(works.indexOf(work));
+	public Set<Integer> search(String query) {
+
+		long start = System.currentTimeMillis();
+		Set<Integer> result = new HashSet<>();
+		// Anstelle eines Query-Parsers hier ein einfaches split:
+		List<String> queries = Arrays.asList(query.split("\\s+"));
+
+		for (String q : queries) {
+			for (Work work : works) {
+				String text = work.getText();
+				List<String> tokens = Arrays.asList(text.split("\\s+"));
+				for (String t : tokens) {
+					if (t.compareTo(q) == 0) {
+						result.add(works.indexOf(work));
+						break;
+					}
 				}
-				
 			}
-			
 		}
-		
+		System.out.println("Dauer: " + (System.currentTimeMillis() - start) + " ms.");
 		return result;
 	}
 
